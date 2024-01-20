91231bc8b2883a76b64d94e437edec4f38e2ec17
JackSparrow9875 <rishiveer07.kv@gmail.com>
2024-01-21 03:40:39 +0530

    rishi3

--- client/src/components/AutoCompleteComponent.jsx
+++ client/src/components/AutoCompleteComponent.jsx
@@ -12,22 +12,24 @@ const AutocompleteComponent = () => {
   };
  
   return (
-    <div className="join center-container">
-      <input
-        className="input input-bordered join-item w-3/4"
-        placeholder="Course Name/code.."
-        autoComplete="on"
-        list="datalistOptions"
-        onChange={handleInputChange}
-      />
-      <datalist id="datalistOptions">
-        {Object.entries(jsonData).map(([code, { title, instructors }]) => (
-          <option key={code} value={`${code} - ${title}`} />
-        ))}
-      </datalist>
-      <button className="btn join-item rounded-r-full" type="submit" onClick={()=>navigate(`/posts/${code}`)}>
-          Search
-      </button>
+    <div className="items-center justify-center h-screen">
+      <div className="join center-container">
+        <input
+          className="input input-bordered join-item w-3/4 bg-slate-100 text-black"
+          placeholder="Enter Course Name or Course Code..."
+          autoComplete="on"
+          list="datalistOptions"
+          onChange={handleInputChange}
+        />
+        <datalist  id="datalistOptions">
+          {Object.entries(jsonData).map(([code, { title, instructors }]) => (
+            <option key={code} value={`${code} - ${title}`} />
+          ))}
+        </datalist>
+        <button className="btn join-item rounded-r-full text-white hover:text-" type="submit" onClick={()=>navigate(`/posts/${code}`)}>
+            Search
+        </button>
+      </div>
     </div>
   );
 };
--- client/src/components/Navbar2.jsx
+++ client/src/components/Navbar2.jsx
@@ -2,33 +2,86 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ThemeToggle from "./ThemeToggle";
 import { Outlet } from "react-router-dom";
-import { NavLink } from "react-router-dom";
-const Navbar = () => {
+import { useContext } from "react";
+import { AuthContext } from "../context/auth-context";
+
+const Navbar2 = () => {
+  const auth = useContext(AuthContext);
+
   return (
     <>
-    <header className="bg-white border-4 rounded-full shadow-2xl">
-        <nav className="flex justify-between items-center w-full mx-auto">
+      <header>
+        <div className="navbar bg-white shadow-md px-3 rounded">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              {/* <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow  rounded-box w-52">
+                <li>
+                  <Link to="/">Item1</Link>
+                </li>
+                <li>
+                  <div className="bg-black text-white" to="/">
+                    Parent
+                    <ul className="p-2 text-black">
+                      <li><a>Submenu 1</a></li>
+                      <li><a>Submenu 2</a></li>
+                    </ul>
+                  </div>
+                </li>
+                <li><a>Item 3</a></li>
+              </ul> */}
+            </div>
+            <Link to="/">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/en/e/e1/International_Institute_of_Information_Technology%2C_Hyderabad_logo.png"
+                alt="IIITH Logo"
+                className="w-40 h-auto"
+              />
+            </Link>
+          </div>
+          {/* <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              <li><a>Item 1</a></li>
+              <li>
+                <details>
+                  <summary>Parent</summary>
+                  <ul className="p-2">
+                    <li><a>Submenu 1</a></li>
+                    <li><a>Submenu 2</a></li>
+                  </ul>
+                </details>
+              </li>
+              <li><a>Item 3</a></li>
+            </ul>
+          </div> */}
+          <div className="navbar-end">
             <div>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/0/0d/IIIT-H_logo.png" alt="..."></img>
+              <a className="btn mr-3 text-white">Register</a>
             </div>
-            <div className="md:static absolute bg-white md:min-h-fit min-h-60vh left-0 -top-100 md:w-auto w-full flex items-center px-5">
-                <ul className="flex md:flex-row flex-col md:items-center md:gap-4vm gap-8">
-                    <li>
-                        <Link to='/' className="hover:text-gray-400">Add New Course</Link>
-                    </li>
-                    <li>
-                        <Link to='/'>Login</Link>
-                    </li>
-                    <li>
-                        <Link to='/'>Register</Link>
-                    </li>
-                </ul>
+            <div>
+              <a className="btn text-white">Log In</a>
             </div>
-        </nav>
-    </header>
-    <Outlet/>
+          </div>
+        </div>
+      </header>
+      <Outlet />
     </>
   );
 };
 
-export default Navbar;
+export default Navbar2;
